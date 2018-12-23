80b24fe56b62f17352415250566ad8555cd694c8
Evan Procter <evanprocter@gmail.com>
2018-12-23 11:34:11 -0500

    added integar variables and performed simple math

--- src/Hello.java
+++ src/Hello.java
@@ -4,5 +4,15 @@ public class Hello {
 
         System.out.println("Hello, Evan!");
 
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int grandTotal = 1000 - myTotal;
+
+        System.out.println(grandTotal);
+
     }
 }
